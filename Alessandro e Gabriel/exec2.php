10e533bd962b985efb78e754986ccca539cf513b
Alezin777 <alessandrogalvao37@gmail.com>
2024-04-08 20:21:04 -0300

    Atividade avaliativa

--- Alessandro e Gabriel/exec2.php
+++ Alessandro e Gabriel/exec2.php
@@ -3,7 +3,7 @@
 //É divisivel por 10, 5, 2
 //Não é divisivel
 
-$num1 = 25;
+$num1 = 100;
 
 
 if ($num1 % 10 == 0 || $num1 % 5 == 0 || $num1 % 2 == 0) {
--- Alessandro e Gabriel/exec5.php
+++ Alessandro e Gabriel/exec5.php
@@ -1,10 +1,40 @@
 <?php 
-	$valo = 0;
-	$cont = 1;
+	$quant = 3;
+	$altura1 = 1.70;
+	$altura2 = 1.60;
+	$altura3 = 1.50;
 
-	while($cont <=10){
-		echo $cont;
-		$cont++;
+	$maioraltura = 0;
+	$menoraltura = 0;
+
+	$media = 0;
+	$soma = 0;
+
+	if ($altura1 > $altura2 && $altura1 > $altura3 ){
+		$maioraltura = $altura1;
+	}elseif ($altura2 > $altura1 && $altura2 > $altura3){
+		$maioraltura = $altura2;
+	}elseif ($altura3 > $altura1 && $altura3 > $altura2){
+		$maioraltura = $altura3;
 	}
+
+	if ($altura1 < $altura2 && $altura1 < $altura3){
+		$menoraltura = $altura1;
+	}elseif ($altura2 < $altura1 && $altura2 < $altura3){
+		$menoraltura = $altura2;
+	}elseif ($altura3 < $altura1 && $altura3 < $altura2){
+		$menoraltura = $altura3;
+	}
+
+	echo "esta é a maior altura ". $maioraltura . " e esta é a menor altura " . $menoraltura . "<br>";
+
+	function Media($altura1, $altura2, $altura3, $quant){
+		$soma = $altura1 + $altura2 + $altura3;
+		$media = ($soma / $quant);
+	}
+
+	Media($soma, $quant);
+	printf("A media de altura de mulheres é " . $media);
+	
 
  ?>
